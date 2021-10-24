867affcb7428929c0838d24feecfe8aea74d7d7a
Deepak <deepakthakur2051@gmail.com>
2021-10-24 13:09:58 +0530

    Product model created

---
+++ backend/Models/ProductModel.js
@@ -0,0 +1,78 @@
+import mongoose from 'mongoose';
+
+const {Schema } = mongoose;
+
+
+// Individaul reviews by user
+const reviewsSchema = Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    rating : {
+        type : Number,
+        required : true
+    },
+    comment : {
+        type : String,
+        required : true
+    }
+}, {timestamps : true});
+
+const productSchema = Schema({
+
+    // Only admin can create product
+
+    user :{
+
+        type :mongoose.Schema.Types.ObjectId,
+        required : true,
+        ref : 'User'
+
+    },
+    name :{
+        type : String,
+        required :true 
+    },
+    brand :{
+        type : String,
+        required :true,
+        unique : true 
+    },
+    category :{
+        type : String,
+        required :true  
+    },
+    description :{
+        type : String,
+        required :true  
+    },
+    reviews :[reviewsSchema],
+    rating :{
+        type : Number,
+        required :true,
+        default : 0  
+    },
+    numReviews :{
+        type : Number,
+        required :true,
+        default :0  
+    },
+    price :{
+        type : Number,
+        required :true,
+        default :0  
+    },
+    countInStock :{
+        type : Number,
+        required :true,
+        default :0  
+    },
+}, {
+    timestamps : true
+});
+
+
+const  Product  = mongoose.model('Product' , productSchema);
+
+export default Product;

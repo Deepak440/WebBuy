05dc958e4ed8b3503c93cdf8f07c8f0c23d97f86
Deepak <deepakthakur2051@gmail.com>
2022-01-07 07:52:53 +0530

    Fetching order details from the backend

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -37,4 +37,22 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
-export {addOrderItems};
+
+
+// @desc : Get order by id
+// @route : Get /orders/:id
+// @access Private
+
+const getOrderById = asyncHandler(async (req, res) => {
+  const  order = await Order.findById(req.params.id).populate('user', 'name email');
+
+  if(order){
+    res.json(order);
+  }else{
+    res.status(404);
+    throw new Error('Order not found');
+  } 
+  
+});
+
+export {addOrderItems, getOrderById};
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { addOrderItems } from "../controller/orderController.js";
+import { addOrderItems, getOrderById } from "../controller/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 
 router.route('/').post(protect , addOrderItems);
-
+router.route('/:id').get(protect , getOrderById);
 export default router;

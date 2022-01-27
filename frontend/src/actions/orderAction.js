0fd6275e2f8f316a4e1629cb83f4861121728953
Deepak <deepakthakur2051@gmail.com>
2022-01-27 21:29:02 +0530

    Order pay action is created

--- frontend/src/actions/orderAction.js
+++ frontend/src/actions/orderAction.js
@@ -5,6 +5,9 @@ import {
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
   ORDER_DETAILS_FAIL,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
+  ORDER_PAY_FAIL,
 } from "../constants/orderConstants";
 
 import axios from 'axios';
@@ -74,4 +77,40 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
     });
   }
 };
+
+
+export const payOrder = (orderId , paymentResult) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_PAY_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        'Content_Type' : 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.put(`/api/orders/${orderId}`,paymentResult,config);
+
+    dispatch({
+      type: ORDER_PAY_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ORDER_PAY_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.message.data.message
+          : error.message,
+    });
+  }
+};
+
+
 
